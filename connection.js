4f2013879c0ee646147df79c43ea828f8f8663de
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2025-04-27 00:33:41 +0700

    deploy

--- connection.js
+++ connection.js
@@ -1,18 +1,17 @@
 import dotenv from 'dotenv'
 import mongoose from "mongoose"
 
-const env = dotenv.config().parsed
-
-
+dotenv.config()
 
 const connection = () => {
-mongoose.connect(env.MONGODB_URI , {
-    dbName: env.MONGODB_NAME
+  mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`, {
+    dbName: process.env.MONGODB_NAME,
   })
+
   const conn = mongoose.connection
-  conn.on('error', console.error.bind(console, 'Connection error :'))
+  conn.on('error', console.error.bind(console, 'Connection error: '))
   conn.once('open', () => {
-    console.log(`Connected to MongoDB Database: ${env.MONGODB_NAME}`)
+    console.log(`Connected to MongoDB Database: ${process.env.MONGODB_NAME}`)
   })
 }
 
